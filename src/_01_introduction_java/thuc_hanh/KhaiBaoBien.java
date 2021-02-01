9a4e444c13fe8df8d971521c996310b6438ae5da
sirrin1991 <sirrin1991@gmail.com>
2021-02-01 13:54:05 +0700

    commit bai thuc hanh cua bai 1

---
+++ src/_01_introduction_java/thuc_hanh/KhaiBaoBien.java
@@ -0,0 +1,12 @@
+package _01_introduction_java.thuc_hanh;
+
+public class KhaiBaoBien {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+    }
+}
